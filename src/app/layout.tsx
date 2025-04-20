f0031bd1f7276c82317884c2a2e7a6bc68035034
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-04-20 12:19:25 +0200

    feat(layout): add Geist Mono font and update layout styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,24 @@
 import type React from 'react';
+
 import type { Metadata } from 'next';
-import { Geist } from 'next/font/google';
+import { Geist, Geist_Mono } from 'next/font/google';
+
 import { ThemeProvider } from '@/components/theme-provider';
+
 import './globals.css';
 
-const geist = Geist({ subsets: ['latin'] });
+const geistSans = Geist({
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
-	title: 'EagleLink | Share Your World',
+	title: 'EagleLink',
 	description: 'All your important links in one beautiful, customizable page',
 };
 
@@ -17,8 +28,8 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en' suppressHydrationWarning>
-			<body className={geist.className}>
+		<html lang="en" suppressHydrationWarning>
+			<body className={`${geistSans.className} ${geistMono.variable} antialiased`}>
 				<ThemeProvider>{children}</ThemeProvider>
 			</body>
 		</html>
