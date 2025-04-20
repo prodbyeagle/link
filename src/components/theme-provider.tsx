be7194dc4f9a907928fef6e3b7c5b2bd6779bfeb
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-04-20 12:19:53 +0200

    fix(theme): change default theme to dark in ThemeProvider component

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
 	return (
-		<NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+		<NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
 			{children}
 		</NextThemesProvider>
 	);
